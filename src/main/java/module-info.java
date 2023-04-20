e9f2245a7053651f7ad44bdf2f58b9c83e331d50
JonStu <jonmurstu@gmail.com>
2023-04-20 23:38:18 +0100

    Presentable Edition
    The first presentable edition of the Bloom App

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module uk.ac.liverpool.groupsoftwareproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens uk.ac.liverpool.groupsoftwareproject to javafx.fxml;
-    exports uk.ac.liverpool.groupsoftwareproject;
-}
